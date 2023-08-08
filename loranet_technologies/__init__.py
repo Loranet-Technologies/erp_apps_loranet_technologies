a619345dec8a9be60bbec0f4ef8ca6a5c14a7336
root <root@erp.loranet.my>
2023-08-08 10:28:10 +0800

    api get user

--- loranet_technologies/__init__.py
+++ loranet_technologies/__init__.py
@@ -1,3 +1,19 @@
 
 __version__ = '0.0.1'
 
+import frappe
+
+@frappe.whitelist(allow_guest=True) 
+def get_user_list():
+    user = frappe.get_all('User')
+    obj = []
+    for x in user:
+          o = frappe.get_doc('User',x)
+          print('\nemail:',x.name)
+          # print(o.phone)
+          a = {
+            'email': x.name,
+            'phone': o.phone
+          }
+          obj.append(a)
+    return obj
